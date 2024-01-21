5e7c6f56d617e62a7281f9a4d93f8f387b28e5ec
Anthony Re <anthonyjosephre@gmail.com>
2024-01-21 04:33:04 -0500

    cleanup; refactored plugin to only deal with LaTeX input

--- server/tex-server.js
+++ server/tex-server.js
@@ -5,33 +5,45 @@
 
 // TODO: Write in Go (or something) containerize, all that jazz as a separate
 // project. Also support websockets.
+// TODO: Proper Logging / Error handling
 
 // NOTE: The available tooling for this is decidedly dated. Sometime in the
 // future when I actually become proficient at programming, I should consider
 // writing a suite of tools that enables streaming rather than excessive file
 // I/O.
 
-import { createServer, STATUS_CODES } from 'http';
-
+import { rm } from 'node:fs/promises';
 import { spawn } from 'child_process';
-
 import { createReadStream } from 'fs';
+
+import {
+  createServer,
+  STATUS_CODES
+} from 'http';
 
 const HOST = process.env.HOST || '0.0.0.0';
 const PORT = process.env.PORT || '8080';
 
 const cacheTTL = 3600 * 24;
 
+/** slug generates a random integer to be used as a prefix when outputting TeX
+  * files. This is done to avoid a race condition between TeX writing the same
+  * output file before pdf2svg has completed its work. */
+const slug = () =>
+  Math.floor(Math.random() * 1e4)
+
 const server = createServer({
   keepAlive: true
 });
 
-// TODO: Clean this mess up.
-server.on('request', (req, res) => {
-  res.setHeader('Accept', 'text/plain');
+const setCors = res => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, POST');
+  res.setHeader('Access-Control-Max-Age', cacheTTL);
+};
 
-  if (req.method === 'OPTIONS') {
-    res.writeHead(200, {
+const optionsHandler = (req, res) => {
+   res.writeHead(200, {
       'Access-Control-Allow-Origin': '*',
       'Access-Control-Allow-Methods': ['OPTIONS', 'POST'],
       'Access-Control-Allow-Headers': [
@@ -43,11 +55,21 @@ server.on('request', (req, res) => {
     });
 
     return res.end();
+};
+
+const failedSpawn = (cmd, err) => {
+  console.error(err);
+}
+
+// TODO: Clean this mess up.
+server.on('request', (req, res) => {
+  res.setHeader('Accept', 'text/plain');
+
+  if (req.method === 'OPTIONS') {
+    return optionsHandler(req, res);
   }
 
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, POST');
-  res.setHeader('Access-Control-Max-Age', cacheTTL);
+  setCors(res);
 
   if (req.method !== 'GET') {
     res.statusCode = 405;
@@ -59,7 +81,8 @@ server.on('request', (req, res) => {
   // Remove the leading slash from the url.
   const latex = decodeURIComponent(req.url).slice(1);
 
-  const tex = spawn('pdflatex');
+  const prefix = slug();
+  const tex = spawn('pdflatex', [`-jobname=${prefix}`]);
 
   // SUPER SECURE
   tex.stdin.write(`${latex}`);
@@ -76,7 +99,6 @@ server.on('request', (req, res) => {
   // Check for successful TeX output, otherwise return an error to the client
   // and some placeholder HTML indicating an error.
   tex.on('exit', (code, signal) => {
-
     if (code) {
       console.error(`TeX exited with code: ${code} and signal: ${signal}`);
 
@@ -84,7 +106,7 @@ server.on('request', (req, res) => {
       return res.end(`${STATUS_CODES[500]}\n`);
     }
 
-    const convert = spawn('pdf2svg', ['texput.pdf', 'output.svg']);
+    const convert = spawn('pdf2svg', [`${prefix}.pdf`, `${prefix}.svg`]);
 
     convert.stderr.on('data', (err) => {
       console.error('pdf2svg Error:', err.toString());
@@ -99,7 +121,7 @@ server.on('request', (req, res) => {
       }
 
       // TODO: Elegant way to handle errors.
-      const svgStream = createReadStream(`output.svg`);
+      const svgStream = createReadStream(`${prefix}.svg`);
 
       res.writeHead(200, { 'content-type': 'image/svg+xml' });
 
--- src/main.ts
+++ src/main.ts
@@ -2,50 +2,17 @@
   * This plugin enables full LaTeX to be rendered in Obsidian's reading mode.
   * In order to work, the author must have a local TeX installation.
   *
-  * TODO: Write TeX service that provides TeX as SVGs.
   * TODO: Learn how to properly process text. (Alg/Compiler book, etc.) */
 
-// bro lmao.
-// fetch("https://i.upmath.me/svg/%0A%5Cbegin%7Balign%7D%0Aa%5E2%20%2B%20b%5E2%20%26%3D%20c%5E2%20%5Ctet%0A%5Cend%7Balign%7D%0A", {
-//   "headers": {
-//     "accept": "*/*",
-//     "accept-language": "en-US,en;q=0.9",
-//     "sec-ch-ua": "\"Not_A Brand\";v=\"8\", \"Chromium\";v=\"120\", \"Google Chrome\";v=\"120\"",
-//     "sec-ch-ua-mobile": "?0",
-//     "sec-ch-ua-platform": "\"Linux\"",
-//     "sec-fetch-dest": "empty",
-//     "sec-fetch-mode": "cors",
-//     "sec-fetch-site": "same-site",
-//     "Referer": "https://upmath.me/",
-//     "Referrer-Policy": "strict-origin-when-cross-origin"
-//   },
-//   "body": null,
-//   "method": "GET"
-// });
-
 import { Plugin } from 'obsidian';
-
-import markdownit from 'markdown-it';
 
 const delimiter = '$$';
 const offset = delimiter.length;
 
-/** Content represents the mutually exclusive contiguous blocks of content that
-  * exist in the input file. */
-type Content =
-  | {t: 'latex'; text: string}
-  | {t: 'markdown'; text: string};
-
-const mdBlock = (text: string): Content => ({
-    t: 'markdown', text
-});
-
-const latexBlock = (text: string): Content => ({
-    t: 'latex', text
-});
-
 /** formatLatexRequest wraps [latex] in a minimal structure needed for TeX to
-  * parse the snippet as a standalone document. */
+  * parse the snippet as a standalone document.
+  *
+  * TODO: Use standalone document class (amsmath). */
 const formatLatexRequest = (latex: string) =>
     encodeURIComponent(`\\documentclass{article}
 \\begin{document}
@@ -64,41 +31,46 @@ const requestSVG = (latex: string) =>
             'Content-Type': 'plain/text'
         },
         referrerPolicy: 'no-referrer',
-    }).then(res => res.text());
+    }).then(res => res.text())
+        .then(txt => txt.slice(txt.indexOf('<svg')));
+
+const createSVG = (raw: string): Node | null => {
+    const svg = document.createElement('svg');
+
+    svg.innerHTML = raw;
+
+    return svg.firstChild;
+};
 
 /** parse recursively sections [content] into a list containing markdown and
   * LaTeX blocks. */
-export const parse = (content: string): Content[] => {
+export const parse = (content: string): string[] => {
     if (!content) {
         return [];
     }
 
-    // What a mess...
-
     const start = content.indexOf(delimiter);
     const end = content.indexOf(delimiter, start+offset);
 
-    // If we encounter a code block 'fence', return as markdown
+    // If we encounter a code block 'fence', skip past it.
     const fenceStart = content.indexOf('```');
     const fenceEnd = content.indexOf('```', start+3);
 
     if (start > fenceStart && end < fenceEnd) {
-        const code = content.slice(0, fenceEnd+3);
-
-        return [mdBlock(code)].concat(parse(content.slice(fenceEnd+3)));
+        return parse(content.slice(fenceEnd+3));
     }
 
-    // If we find a '$$' without a correspondind closing '$$', we assume its
-    // markdown
+    // If we find a '$$' without a corresponding closing '$$', we assume the
+    // rest is markdown
     if (end < 0) {
-        return [mdBlock(content)];
+        return [];
     }
 
     // Poor man's lexing
-    const md = mdBlock(content.slice(0, start));
-    const latex = latexBlock(content.slice(start+offset, end).trim());
+    // const md = mdBlock(content.slice(0, start));
+    const latex = content.slice(start+offset, end).trim();
 
-    return [md, latex].concat(parse(content.slice(end+offset)));
+    return [latex].concat(parse(content.slice(end+offset)));
 };
 
 /** TeXXen looks for embedded LaTeX in a markdown document and uses a local TeX
@@ -107,8 +79,6 @@ export const parse = (content: string): Content[] => {
 export default class MarkTeX extends Plugin {
     async onload() {
         this.registerMarkdownPostProcessor(async (element, context) => {
-            const md = markdownit();
-
             const note = this.app.workspace.getActiveFile();
 
             if (!note) {
@@ -117,13 +87,15 @@ export default class MarkTeX extends Plugin {
 
             const text = await this.app.vault.read(note);
 
-            const content = parse(text).map(block => {
-                return block.t === 'markdown'
-                    ? Promise.resolve(md.render(block.text))
-                    : requestSVG(block.text);
-            });
+            const svgs = await Promise.all(parse(text).map(requestSVG));
 
-            console.log(await Promise.all(content));
+            const svgTags = svgs.map(createSVG);
+
+            const maths = element.findAll('.math');
+
+            if (svgTags[0]) {
+                maths[0].replaceWith(svgTags[0]);
+            }
 
             void(element);
             void(context);
